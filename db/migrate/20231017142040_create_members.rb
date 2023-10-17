3535bb4f2b2f6dac77cb99330a0a8c695000cf30
Sam-2019 <samtmartey@gmail.com>
2023-10-17 15:22:57 +0100

    added member scaffold

--- db/migrate/20231017142040_create_members.rb
+++ db/migrate/20231017142040_create_members.rb
@@ -1,16 +1,16 @@
-# frozen_string_literal: true
-
 class CreateMembers < ActiveRecord::Migration[7.0]
   def change
     create_table :members do |t|
       t.string "uuid", null: false
       t.string "name", null: false
       t.string "phone", null: false
-      t.string "email", null: true
+      t.string "email"
       t.integer "chapel", limit: 2, null: false
       t.date "dob", null: false
-      t.string "photo", null: true
+      t.string "photo"
       t.boolean "active", default: true, null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
 
       t.timestamps
     end
