6cd02754edbb55c9b5a70d84c28ebc64b99acfa1
Sam-2019 <samtmartey@gmail.com>
2023-10-17 12:11:42 +0100

    annotate

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :integer          not null, primary key
+#  active     :boolean          default(TRUE), not null
+#  chapel     :string           not null
+#  dob        :date             not null
+#  email      :string
+#  name       :string           not null
+#  phone      :string           not null
+#  photo      :string
+#  uuid       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Member < ApplicationRecord
 end
